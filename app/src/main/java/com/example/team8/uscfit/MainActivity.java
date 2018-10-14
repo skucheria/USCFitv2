c5b658267ebe3ce1eec130c19401927fa1346a1e
Siddharth Kucheria <kucheria@usc.edu>
2018-10-14 15:08:09 -0700

    Changed some titles and shit

--- app/src/main/java/com/example/team8/uscfit/MainActivity.java
+++ app/src/main/java/com/example/team8/uscfit/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText("To-Do");
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
