1b26992f8e6440869f238e51aab566d997367a82
Sidharth Raguraman <ragurama@usc.edu>
2018-10-14 15:15:43 -0700

    added activities

---
+++ app/src/main/java/com/example/team8/uscfit/Pedometer.java
@@ -0,0 +1,13 @@
+package com.example.team8.uscfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pedometer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedometer);
+    }
+}
