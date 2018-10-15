be8c3a86ffda505750e89b0450b57b653ed56c42
Siddharth Kucheria <kucheria@usc.edu>
2018-10-14 23:18:42 -0700

    added code for sensor in main activity. need to figure out how to make it persist through different views etc

--- app/src/main/java/com/example/team8/uscfit/MainActivity.java
+++ app/src/main/java/com/example/team8/uscfit/MainActivity.java
@@ -7,12 +7,21 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.hardware.*;
 import android.app.Activity;
 import android.content.Intent;
 
-public class MainActivity extends FragmentActivity {
+import com.example.team8.uscfit.pedometer.*;
+
+public class MainActivity extends FragmentActivity implements SensorEventListener, StepListener {
 
     private TextView mTextMessage;
+
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "Number of Steps: ";
+    private int numSteps;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -41,12 +50,38 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 //        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+//        TvSteps.setText(TEXT_NUM_STEPS + numSteps);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
     }
 
 }
