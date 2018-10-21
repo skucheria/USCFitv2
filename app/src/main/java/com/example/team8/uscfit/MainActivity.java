40fa55cca1e01abeb62ba3b1fdfc94c3fc1dcc0c
Alexander Kuo <alexanek@gmail.com>
2018-10-20 19:13:53 -0700

    SWITCHING TABS FINALLY FUCKING WORKS

--- app/src/main/java/com/example/team8/uscfit/MainActivity.java
+++ app/src/main/java/com/example/team8/uscfit/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -17,65 +19,78 @@ import android.view.*;
 import android.content.DialogInterface.OnClickListener;
 import com.example.team8.uscfit.pedometer.*;
 
-public class MainActivity extends FragmentActivity implements SensorEventListener, StepListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
 
     private TextView mTextMessage;
-
     private StepDetector simpleStepDetector;
     private SensorManager sensorManager;
     private Sensor accel;
     private static final String TEXT_NUM_STEPS = "Number of Steps: ";
     private int numSteps;
     private TextView TvStep;
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
 
-                    switch (item.getItemId()) {
-                        case R.id.nav_todo:
-                            selectedFragment = new Todo_Fragment();
-                            break;
-                        case R.id.nav_calories:
-                            selectedFragment = new Calories_Fragment();
-                            break;
-                        case R.id.nav_steps:
-                            selectedFragment = new Steps_Fragment();
-                            break;
-                    }
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
-
-                    return true;
-                }
-            };
+    private BottomNavigationView bottomMenu;
+    private FragmentManager fm;
+    private Fragment f;
+    private FragmentTransaction ft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        simpleStepDetector = new StepDetector();
-        simpleStepDetector.registerListener(this);
-
-
-         TvStep = (TextView) findViewById(R.id.tv_steps);
-
-        numSteps = 0;
-        sensorManager.registerListener(MainActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
-
-        TvStep.setText(TEXT_NUM_STEPS + numSteps);
-
-
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(navListener);
+        bottomMenu = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        fm = getSupportFragmentManager();
+        f = fm.findFragmentById(R.id.fragment_container);
+        ft = fm.beginTransaction();
+        if (f == null) {
+            f = new Steps_Fragment();
+            ft.add(R.id.fragment_container, f);
+        }
+        else {
+            ft.replace(R.id.fragment_container, f);
+        }
+        ft.commit();
+        bottomMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                // if steps button clicked, switch to steps fragment
+                if(menuItem.getItemId() == R.id.nav_steps) {
+                    ft = fm.beginTransaction();
+                    f = new Steps_Fragment();
+                    ft.replace(R.id.fragment_container, f);
+                    ft.addToBackStack(null);
+                    ft.commit();
+                }
+                else if(menuItem.getItemId() == R.id.nav_calories) {
+                    ft = fm.beginTransaction();
+                    f = new Calories_Fragment();
+                    ft.replace(R.id.fragment_container, f);
+                    ft.addToBackStack(null);
+                    ft.commit();
+                }
+                else if(menuItem.getItemId() == R.id.nav_todo) {
+                    ft = fm.beginTransaction();
+                    f = new Todo_Fragment();
+                    ft.replace(R.id.fragment_container, f);
+                    ft.addToBackStack(null);
+                    ft.commit();
+                }
+                return true;
+            }
+        });
     }
+
+//        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+//        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        simpleStepDetector = new StepDetector();
+//        simpleStepDetector.registerListener(this);
+//
+//        //TvStep = (TextView) findViewById(R.id.tv_steps);
+//
+//        numSteps = 0;
+//        sensorManager.registerListener(MainActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+//
+//        TvStep.setText(TEXT_NUM_STEPS + numSteps);
 
     @Override
     public void step(long timeNs) {
@@ -113,5 +128,6 @@ public class MainActivity extends FragmentActivity implements SensorEventListene
 
         }
     }
+
 
 }
--- app/src/main/java/com/example/team8/uscfit/Todo_Fragment.java
+++ app/src/main/java/com/example/team8/uscfit/Todo_Fragment.java
@@ -1,7 +1,6 @@
 package com.example.team8.uscfit;
 
 import android.support.v4.app.Fragment;
-import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +8,9 @@ import android.view.ViewGroup;
 
 
 public class Todo_Fragment extends Fragment {
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.todo_layout, container, false);
     }
