1e8f33cf59b8774b8937d569da1900a710409451
Sidharth Raguraman <ragurama@usc.edu>
2018-10-28 17:45:50 -0700

    added even more tests

---
+++ app/src/test/java/com/example/team8/uscfit/UserUnitTest.java
@@ -0,0 +1,36 @@
+package com.example.team8.uscfit;
+
+import com.example.team8.uscfit.objects.User;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserUnitTest extends User {
+
+    User u = new User();
+
+    @Test
+    public void getBMI_isCorrect(){
+        u.setBmi(50);
+        assertEquals(50, 50, 0.1);
+    }
+
+    @Test
+    public void getName_isCorrect(){
+        u.setName("Praneeth");
+        assertEquals(u.getName(), "Praneeth");
+    }
+
+    @Test
+    public void getWeight_isCorrect(){
+        u.setWeight(30);
+        assertEquals(u.getWeight(), 30);
+    }
+
+    @Test
+    public void getHeight_isCorrect(){
+        u.setHeight(40);
+        assertEquals(u.getHeight(), 40);
+    }
+}
