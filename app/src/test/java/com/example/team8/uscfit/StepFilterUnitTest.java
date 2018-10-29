f17a02c82c42c458035d2d127692b4894e5cb28c
praneeth12098 <pbatte@usc.edu>
2018-10-28 17:36:50 -0700

    finished step filter unit tests

--- app/src/test/java/com/example/team8/uscfit/StepFilterUnitTest.java
+++ app/src/test/java/com/example/team8/uscfit/StepFilterUnitTest.java
@@ -18,4 +18,29 @@ public class StepFilterUnitTest extends StepFilter {
 
         assertEquals(6, sum(arr), 0.1);
     }
+
+    @Test
+    public void norm_isCorrect() {
+        float[] arr = new float[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        assertEquals(3.74, norm(arr), 0.1);
+    }
+
+    @Test
+    public void dot_isCorrect() {
+        float[] arr1 = new float[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
+
+        float[] arr2 = new float[3];
+        arr2[0] = 1;
+        arr2[1] = 2;
+        arr2[2] = 3;
+
+        assertEquals(14, dot(arr1, arr2), 0.1);
+    }
 }
